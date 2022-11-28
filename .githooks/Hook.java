c7f2ab34f17dade6e438458f6cd0d6966bb29802
Gaston Pallas <pallasgaston@gmail.com>
2022-11-28 17:18:08 +0100

    gaston githook tom le naze

--- .githooks/Hook.java
+++ .githooks/Hook.java
@@ -6,11 +6,11 @@ public class Hook {
     public static void main (String[] args) throws IOException {
         String gitMessage = readFirstLine(args[0]);
         System.out.println(gitMessage);
-        if (!gitMessage.startsWith("tom") || gitMessage.startsWith("gaston")) {
-            System.out.println("Commit message must start with 'tom' or 'gaston'");
-            System.exit(1);
+        if (gitMessage.startsWith("tom") || gitMessage.startsWith("gaston")) {
+            System.exit(0);
         }
-        System.exit(0);
+        System.out.println("Commit message must start with 'tom' or 'gaston'");
+        System.exit(1);
     }
 
     public static String readFirstLine(String fileName)throws IOException
